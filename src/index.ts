8f3367d5effe6a0a4bc51ce03d2348a42340793d
med40 <medvind2140@gmail.com>
2022-12-15 21:46:57 +0100

    rebase

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+export function helloWorld() {
+  const message = 'Hello npm package!';
+  return message;
+}
+
+export function goodBye() {
+  const message = 'Goodbye npm package!';
+  return message;
+}
+
+export default {
+  helloWorld,
+  goodBye,
+};
